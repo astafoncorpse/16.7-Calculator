7847d8f21a586fcbc968c92faece7c3aa6989bdd
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-03-09 04:43:30 +0300

    Добавьте файлы проекта.

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using _16._7_Calcukator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+       
+            [Test]
+            public void AdditionalMustReturnCorrectValue()
+            {
+                var calculator = new Calculator();
+                Assert.That(calculator.Additional(50, 10) == 60);
+            }
+
+            [Test]
+            public void SubtractionMustReturnCorrectValue()
+            {
+                var calculator = new Calculator();
+                Assert.That(calculator.Subtraction(300, 10) == 290);
+            }
+
+            [Test]
+            public void MultiplicationMustReturnCorrectValue()
+            {
+                var calculator = new Calculator();
+                Assert.That(calculator.Miltiplication(2,20) == 40);
+            }
+
+            [Test]
+            public void DivisionMustReturnCorrectValue()
+            {
+                var calculator = new Calculator();
+                Assert.That(calculator.Division(200, 10) == 20);
+            }
+
+        }
+    }
+
